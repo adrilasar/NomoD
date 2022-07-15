7dcafa1a6b147d1cdcb884e024832d6e86ee1fac
adrilasar <contact@adrilasar.com>
2022-07-15 13:30:10 +0200

    Añado armaduras Kalcenita #18

---
+++ src/main/java/com/adrilasar/nomod/item/ModArmorMaterials.java
@@ -0,0 +1,73 @@
+package com.adrilasar.nomod.item;
+
+import com.adrilasar.nomod.NomoD;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements IArmorMaterial
+{
+    KALCENITA("kalcenita", 28, new int[]{2, 5, 8, 3}, 19, SoundEvents.ARMOR_EQUIP_GOLD,
+            2.0F, 0.0F, () -> Ingredient.of(ModItems.KALCENITA_INGOT.get()));
+
+    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    ModArmorMaterials(String pName, int pDurabilityMultiplier, int[] pSlotProtections, int pEnchantmentValue,
+                      SoundEvent pSound, float pToughness, float pKnockbackResistance, Supplier<Ingredient> pRepairIngredient) {
+        this.name = pName;
+        this.durabilityMultiplier = pDurabilityMultiplier;
+        this.slotProtections = pSlotProtections;
+        this.enchantmentValue = pEnchantmentValue;
+        this.sound = pSound;
+        this.toughness = pToughness;
+        this.knockbackResistance = pKnockbackResistance;
+        this.repairIngredient = new LazyValue<>(pRepairIngredient);
+    }
+
+    public int getDurabilityForSlot(EquipmentSlotType pSlot) {
+        return HEALTH_PER_SLOT[pSlot.getIndex()] * this.durabilityMultiplier;
+    }
+
+
+    public int getDefenseForSlot(EquipmentSlotType pSlot) {
+        return this.slotProtections[pSlot.getIndex()];
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public SoundEvent getEquipSound() {
+        return this.sound;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+
+    public String getName() {
+        return NomoD.MOD_ID + ":" + this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
