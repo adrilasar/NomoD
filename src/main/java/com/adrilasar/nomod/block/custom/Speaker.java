ff6bc6c59e828511376734d9667e28416322e683
adrilasar <contact@adrilasar.com>
2022-07-15 22:27:41 +0200

    Cambios Altavoz (TODO)

--- src/main/java/com/adrilasar/nomod/block/custom/Speaker.java
+++ src/main/java/com/adrilasar/nomod/block/custom/Speaker.java
@@ -1,27 +1,35 @@
 package com.adrilasar.nomod.block.custom;
 
+import com.adrilasar.nomod.item.custom.SpeakerUsb;
+import com.adrilasar.nomod.sound.ModSounds;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.JukeboxBlock;
+import net.minecraft.command.impl.data.BlockDataAccessor;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.StateContainer;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.state.*;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.ITickList;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class Speaker extends JukeboxBlock
+import javax.annotation.Nullable;
+import java.util.Random;
+
+public class Speaker extends Block
 {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     protected static final VoxelShape S_SHAPE = Block.box(0.0D, 0.0D, 3.0D, 16.0D, 26.0D, 16.0D);
@@ -29,8 +37,15 @@ public class Speaker extends JukeboxBlock
     protected static final VoxelShape W_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 13.0D, 26.0D, 16.0D);
     protected static final VoxelShape N_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 26.0D, 13.0D);
 
-    public Speaker(Properties properties) {
-        super(properties);
+    public static final BooleanProperty PLAYING = BooleanProperty.create("playing");
+
+    public Speaker(Properties p_i48440_1_) {
+        super(p_i48440_1_);
+    }
+
+    @Override
+    public void setPlacedBy(World pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack) {
+        super.setPlacedBy(pLevel, pPos, pState, pPlacer, pStack);
     }
 
     public VoxelShape getShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
@@ -48,22 +63,31 @@ public class Speaker extends JukeboxBlock
 
     @Override
     public ActionResultType use(BlockState pState, World pLevel, BlockPos pPos, PlayerEntity pPlayer, Hand pHand, BlockRayTraceResult pHit) {
-        return ActionResultType.PASS;
-    }
-
-    @Override
-    public void setRecord(IWorld p_176431_1_, BlockPos p_176431_2_, BlockState p_176431_3_, ItemStack p_176431_4_) {
-        super.setRecord(p_176431_1_, p_176431_2_, p_176431_3_, p_176431_4_);
-    }
-
-    @Override
-    public void onRemove(BlockState pState, World pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
-        if (!pState.is(pNewState.getBlock())) {
-            super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+        Item item = pPlayer.getItemInHand(pHand).getItem();
+        if (item instanceof SpeakerUsb) {
+            pLevel.playLocalSound(pPos.getX(), pPos.getY(), pPos.getZ(), SoundEvents.WOODEN_BUTTON_CLICK_OFF, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+            pLevel.getBlockEntity(pPos).getUpdateTag().putString("sound", pPlayer.getItemInHand(pHand).getTag().getString("sound"));
+            pLevel.getBlockEntity(pPos).getUpdateTag().putString("duration", pPlayer.getItemInHand(pHand).getTag().getString("duration"));
+            return ActionResultType.sidedSuccess(pLevel.isClientSide);
+        } else {
+            return ActionResultType.PASS;
         }
     }
 
-    //FACING
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState pState, World pLevel, BlockPos pPos, Random pRand) {
+        if (pRand.nextInt(20) == 0 /*&& pLevel.getBlockEntity(pPos).getUpdateTag().contains("sound")*/) {
+            if(!pState.getValue(PLAYING)) {
+                pState.setValue(PLAYING, true);
+                //TODO Cambiar por sonido para todos (no local)
+//                pLevel.playLocalSound(pPos.getX(), pPos.getY(), pPos.getZ(), sound, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+            }
+        }
+    }
+// TODO Clase que con la duracion (NBT) ponga isPlaying a false cuando se acabe la cancion
+
+//FACING
 
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext pContext) {
@@ -84,5 +108,11 @@ public class Speaker extends JukeboxBlock
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> pBuilder) {
         super.createBlockStateDefinition(pBuilder);
         pBuilder.add(FACING);
+        pBuilder.add(PLAYING);
     }
+
+    /*@Override
+    public void playSound(@Nullable PlayerEntity pPlayer, BlockPos pPos, SoundEvent pSound, SoundCategory pCategory, float pVolume, float pPitch) {
+
+    }*/
 }
